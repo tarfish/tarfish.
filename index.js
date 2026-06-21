51527b09e7ac12d4b457222aeee053f2a0c8dec9
Sarvesh Neelraj <sarveshneel@gmail.com>
2026-06-21 19:23:04 +0400

    added more features

--- index.js
+++ index.js
@@ -20,9 +20,26 @@ app.command("/tarfish-help", async ({ ack, respond }) => {
   await respond({
     text: `
 *🤖 general commands:*
-/tarfish-ping - check bot latency
-/tarfish-help - open this help menu`
+\`/tarfish-ping\` - check bot latency
+\`/tarfish-help\` - open this help menu
+`
   });
+});
+
+app.command("/tarfish-greet" , async ({ command, ack, respond }) => {
+  await ack();
+  await respond({text: `yo, <@${command.user_id}>!`});
+});
+
+app.event("member_joined_channel", async ({ event, client }) => {
+  try {
+    await client.chat.postMessage({
+      channel: "C08LLBLUVPH",
+      text: `welcome to <#C08LLBLUVPH>, <@${event.user}>!`
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 (async () => {
