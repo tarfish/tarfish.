1290f924ad5b16f7c5d7c8733322826338e96c8d
Sarvesh Neelraj <sarveshneel@gmail.com>
2026-06-30 20:36:26 +0400

    made the join thing work and other features

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const { App } = require("@slack/bolt");
 const sarveshId = "U081D39L4MD";
-const targetChannelId = "C0BCHFZBYMT";
+const targetChannelIds = ["C0BCHFZBYMT", "C08LLBLUVPH"];
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -15,7 +15,7 @@ app.command("/tarfish-ping", async ({ command, ack, respond }) => {
   const start = Date.now();
   await ack();
   const latency = Date.now() - start;
-  await respond({ text: `Pong!\nLatency: ${latency}ms` });
+  await respond({ text: `pong!\nlatency: ${latency}ms` });
 });
 
 app.command("/tarfish-help", async ({ ack, respond }) => {
@@ -37,19 +37,19 @@ app.command("/tarfish-greet" , async ({ command, ack, respond }) => {
 
 app.event("member_joined_channel", async ({ event, client }) => {
   try {
-    if (event.channel !== targetChannelId) {
+    if (!targetChannelIds.includes(event.channel)) {
       return;
     }
 
-    await client.chat.postMessage({
-      channel: targetChannelId,
-      text: `welcome to <#${targetChannelId}>, <@${event.user}>!`,
+    const welcomeMessage = await client.chat.postMessage({
+      channel: event.channel,
+      text: `welcome to <#${event.channel}>, <@${event.user}>!`,
       blocks: [
         {
           type: "section",
           text: {
             type: "mrkdwn",
-            text: `welcome to <#${targetChannelId}>, <@${event.user}>!`
+            text: `welcome to <#${event.channel}>, <@${event.user}>!`
           }
         },
         {
@@ -66,26 +66,33 @@ app.event("member_joined_channel", async ({ event, client }) => {
           ]
         }
       ]
-    });
+});
+
+    await client.chat.postMessage({
+      channel: event.channel,
+      thread_ts: welcomeMessage.ts,
+      text: `here we have a very long talk about this channel which you probably didn't even sign up for :hs:\n\n*✦ channel lore:*\nthis channel was made in april 2025 when hackclub's <#C088UF12N1Z> event was happening. it was mainly used back then to communicate with his close friends about the chinese culture and tourism blah blah\n\n*✦ channel use:*\nit's mostly used by sarvesh to talk about his life and have fun with huddles (maybe one day when he decides to actually reveal his voice :wink:)\n\n*✦ THANK YOUS :YAY::*\n(from yours truly)\nthank you so much to the people who keep me on track and prevent me from procrastinating from the things that actually matter, a HUGE thank you to the friends i made along the way, and lastly, thank you to *YOU* to taking the effort to join the channel and support me!`
+})
+
   } catch (error) {
     console.error(error);
   }
+
 });
 
 app.action("ping_sarvesh", async ({ ack, body, client }) => {
   await ack();
   try {
     await client.chat.postMessage({
-      channel: targetChannelId,
-      text: `SARVESH GET HERE NOWW <@${sarveshId}>!`
+      channel: body.channel.id,
+      thread_ts: body.message.ts,
+      text: `SARVESH GET HERE NOW! <@${sarveshId}>! (pinged by <@${body.user.id}>)`
     });
-  await respond({
-  replace_original: true,
-});
   } catch (error) {
     console.error(error);
   }
 });
+
 
 (async () => {
   await app.start();
